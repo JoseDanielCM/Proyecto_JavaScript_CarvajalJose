b9d74849ddbf3ee90249cc651618a9e4cca72b79
JoseDanielCM <jodacarmon@gmail.com>
2024-08-22 17:02:50 -0500

    feat: :sparkles: añadir elementos y formulario para obtener datos del recurso

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
+// ESTRELLAS //
+
+let estrellas = document.querySelectorAll(".stars");
+for (const star of estrellas) {
+    star.addEventListener("click",()=>{
+        valorStar = Number(star.getAttribute("value"))
+        document.getElementById("formPuntuacion").setAttribute("value",valorStar)
+        for (const starR of estrellas) {
+            starR.classList.remove("starActive");
+        }
+
+        for (const starON of estrellas) {
+            if (starON.getAttribute("value")<=valorStar) {
+                starON.classList.add("starActive")
+            }
+        }
+    })
+}
+
+// ******************************************************
+
 let items = document.querySelectorAll(".dropdown-item")
 
 items.forEach(element => {
     element.addEventListener("click",()=>{
-        console.log(element.innerHTML);
+        let valueEscogido = element.innerHTML
+        let objetivo = element.getAttribute("value")
         
+
+        let elementoObjetivo = document.getElementById(objetivo)
+        elementoObjetivo.innerHTML = valueEscogido
+        elementoObjetivo.classList.remove("btn-danger")
+        elementoObjetivo.classList.remove("btn-succes")
+        elementoObjetivo.classList.remove("btn-warning")
+
+        let color = element.getAttribute("color")
+        
+        if (color !== null) {
+            elementoObjetivo.classList.remove("btn-secondary")
+            elementoObjetivo.classList.add(`btn-${color}`)
+        }
     })
 });
 
-    
