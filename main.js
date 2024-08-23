51f8e61e7c9f8c480dd8a10f0b7dfa7da970c602
JoseDanielCM <jodacarmon@gmail.com>
2024-08-23 07:46:23 -0500

    feat: :sparkles: checklist a generos

--- main.js
+++ main.js
@@ -42,3 +42,34 @@ items.forEach(element => {
     })
 });
 
+// que el dropdown no se cierre al dar click en labels de adentro
+document.querySelectorAll('.dropdown-menu').forEach(function (dropdown) {
+    dropdown.addEventListener('click', function (event) {
+        event.stopPropagation();
+    });
+});
+
+//
+
+function updateButtonText() {
+    const checkboxes = document.querySelectorAll('.form-check-input:checked');
+    const button = document.getElementById('formGenero');
+    let selected = [];
+
+    checkboxes.forEach((checkbox) => {
+        selected.push(checkbox.value);
+    });
+
+    // Si hay opciones seleccionadas, mostrarlas en el botón; si no, mostrar el texto por defecto.
+    if (selected.length > 0) {
+        button.textContent = selected.join(', ');
+    } else {
+        button.textContent = 'Géneros';
+    }
+}
+
+// Escuchar cambios en los checkboxes
+document.querySelectorAll('.form-check-input').forEach((checkbox) => {
+    checkbox.addEventListener('change', updateButtonText);
+});
+
